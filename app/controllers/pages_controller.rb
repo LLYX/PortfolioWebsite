308c0de9de48f47a02efd2e3def17fab835c437a
Leon Xu <leon.xu@mail.utoronto.ca>
2016-06-07 15:43:37 -0400

    Add app files
    Add website rails code for move to Github

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+    def welcome
+    end
+    
+    def portfolio
+    end
+    
+    def resume
+    end
+    
+    def about
+    end
+    
+    def contact
+    end
+end
